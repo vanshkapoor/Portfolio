c8f342966f4b04a66bbf36938c6251cff9630e22
vanshkapoor <vanshkapoorvk7@gmail.com>
2020-07-23 01:10:18 +0530

    ui improved

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -3,8 +3,10 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <p className="mt-0 mb-0 pt-2">Life is just another simulation</p>
-      <p className="mt-0 mb-0 pt-2 pb-2">Vansh Kapoor</p>
+      <p className="mt-0 mb-0 pt-4">Life is just another simulation</p>
+      {/* <p className="mt-0 mb-0 pt-2 pb-2">Vansh Kapoor</p> */}
+      <img src={require("../../V1.png")} width="40px" />
+      <p>Vansh Kapoor&copy; 2020</p>
     </>
   );
 };
--- src/components/hero/main.jsx
+++ src/components/hero/main.jsx
@@ -4,7 +4,9 @@ import "./hero.css";
 const Hero = () => {
   return (
     <div className="cont ">
-      <h3 className="pt-4">Vansh</h3>
+      <h3 className="pt-4">
+        <img width="50px" src={require("../../V1.png")} />
+      </h3>
 
       <br />
       <br />
@@ -17,24 +19,32 @@ const Hero = () => {
       <div>
         <div className="row">
           <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-            <p className="mb-0">Hii! I'm</p>
+            <p className="mb-0" style={{ fontSize: "1.2rem" }}>
+              Hii! I'm
+            </p>
             <h1 className="mt-0 mb-2" style={{ fontSize: "4rem" }}>
               Vansh Kapoor
             </h1>
-            <h3 className="mt-0">A Full Stack Developer | Programmer</h3>
-            <button href="#detailer" className="mt-2 mb-2" style={{ fontSize: "1.2rem" }}>
+            <h3 style={{ color: "#616161" }} className="mt-0">
+              A Full Stack Developer | Programmer
+            </h3>
+            <button
+              href="#detailer"
+              className="mt-2 mb-2 pl-4 pr-4"
+              style={{ fontSize: "1.2rem" }}
+            >
               Download Resume
             </button>
-            <div className="d-flex pt-2">
+            <div className="d-flex pt-2" style={{ color: "grey" }}>
               <i className="fab fa-twitter fontsz-15" />
-              <i className="fab fa-github pl-2 fontsz-15" />
-              <i className="fab fa-linkedin pl-2 fontsz-15" />
+              <i className="fab fa-github pl-4 fontsz-15" />
+              <i className="fab fa-linkedin pl-4 fontsz-15" />
             </div>
           </div>
 
           <div
             className="col-lg-6 col-md-6 col-sm-12 col-xs-12"
-            style={{ textAlign: "center" }}
+            style={{ textAlign: "center", position: "relative" }}
           >
             <img
               width="50%"
@@ -42,6 +52,10 @@ const Hero = () => {
               alt="me"
               src="https://vanshkapoor.github.io/pic.jpg"
             />
+            {/* <br /> */}
+            <img src={require("../../boxes.png")} width="300px" className="boxesimg2" />
+            <br />
+            <img src={require("../../boxes.png")} width="100%" className="boxesimg" />
           </div>
         </div>
       </div>
